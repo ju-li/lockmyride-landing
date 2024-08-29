bafa8a564e339393ce9dd3606ba3da436313e00c
Ju Li <ju.li@stern.nyu.edu>
2024-08-28 21:50:11 -0400

    v1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { cn } from '@/lib/utils'
 import HomeComp from "../components/home";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const fontHeading = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-heading',
+})
+
+const fontBody = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-body',
+})
+
 export default function Home() {
   return (
-    <HomeComp />
+    <div className={cn(
+      'antialiased',
+      fontHeading.variable,
+      fontBody.variable
+    )}>
+      <HomeComp />
+    </div>
   );
 }
